9d5ff454a32d729279663b945cd46eb4204d012f
dennismuuo044@gmail.com <dennismuuo044@gmail.com>
2024-06-22 15:31:41 +0300

    updated file

--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -1,13 +1,41 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import get_object_or_404, render
+from .models import Customer, Product
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'Ecommerce/home.html')
 def product_list(request):
     products = Product.objects.all()
     context = {
-        'products':products, 
+        'products':products 
         }
     return render(request,'Ecommerce/product_list.html',context)
+
+def product_detail(request, pk):
+    product = get_object_or_404(product, pk=pk)
+    context = {
+        'product':product 
+        }
+    return render(request, 'Ecommerce/product_detail.html', context)
+
+def customer_list(request):
+    customers = Customer.objects.all()
+    context = {
+        'customers':customers
+    }
+    return render(request,'Ecommerce/customer_list.html', context)
+
+def customer_detail(request,pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    context ={
+        'customer':customer
+    }
+    return render(request,'Ecommerce/customer_detail.html', context)
+
+
+ 
+
 
     
     
--- plp_Academy/urls.py
+++ plp_Academy/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('Ecommerce.urls')),
+    path('Ecommerce/',include('Ecommerce.urls')),
 ]
